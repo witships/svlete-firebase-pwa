a8e3e62947c6afb66025e33c8faa5848d64dc89d
witships <witships@gmail.com>
2024-02-21 21:19:18 +0900

    swをネットワークファーストに変更

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', function (event) {});
+// アクションなし
+// self.addEventListener("fetch", function (event) {})
+
+// ネットワークファースト
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		fetch(event.request).catch(function () {
+			return caches.match(event.request);
+		}),
+	);
+});
